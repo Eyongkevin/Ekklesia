c7bd14f2dd28efb2dcaee45736308480754bde59
Eyongkevin <tonyparkerkenz@gmail.com>
2026-03-27 12:40:35 +0100

    Set alembic with postgresql and initialize church table

---
+++ backend/app/models/church.py
@@ -0,0 +1,36 @@
+import uuid
+from datetime import datetime
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import String, func, DateTime
+
+from app.db.base import Base
+
+
+class Church(Base):
+    __tablename__ = "churches"
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4
+    )
+
+    name: Mapped[str] = mapped_column(
+        String,
+        unique=True, 
+        nullable=False)
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now()
+    )
+
+    modified_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now()
+    )
+
+    def __repr__(self) -> str:
+        return f'<Church {self.name}>'
